70a77c9e73c06a08290760730f4022d6c47f02bc
AgustinVallejo <agustin.vallejo@udea.edu.co>
2026-03-23 17:01:35 -0500

    Removing the generic types shenanigans

--- js/common/model/BetaDecayModel.ts
+++ js/common/model/BetaDecayModel.ts
@@ -6,7 +6,7 @@
  */
 
 import Property from '../../../../axon/js/Property.js';
-import NuclearDecayModel, { NuclearDecayModelOptions } from '../../../../nuclear-decay-common/js/model/NuclearDecayModel.js';
+import NuclearDecayModel, { NuclearDecayModelOptions, SelectableIsotopes } from '../../../../nuclear-decay-common/js/model/NuclearDecayModel.js';
 import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
 
 type SelfOptions = EmptySelfOptions;
@@ -17,11 +17,11 @@ export type BDSelectableIsotopes = ( typeof BDSelectableIsotopesValues )[ number
 
 export type BetaDecayModelOptions = SelfOptions & NuclearDecayModelOptions;
 
-export default class BetaDecayModel extends NuclearDecayModel<BDSelectableIsotopes> {
+export default class BetaDecayModel extends NuclearDecayModel {
 
   public override readonly selectableIsotopes: BDSelectableIsotopes[];
 
-  public override readonly selectedIsotopeProperty: Property<BDSelectableIsotopes>;
+  public override readonly selectedIsotopeProperty: Property<SelectableIsotopes>;
 
   public constructor( providedOptions: BetaDecayModelOptions ) {
     const options = optionize<SelfOptions, EmptySelfOptions, BetaDecayModelOptions>()( {
@@ -32,6 +32,6 @@ export default class BetaDecayModel extends NuclearDecayModel<BDSelectableIsotop
 
     this.selectableIsotopes = [ ...BDSelectableIsotopesValues ];
 
-    this.selectedIsotopeProperty = new Property<BDSelectableIsotopes>( 'hydrogen-3' );
+    this.selectedIsotopeProperty = new Property<SelectableIsotopes>( 'hydrogen-3' );
   }
 }
--- js/common/view/BetaDecayScreenView.ts
+++ js/common/view/BetaDecayScreenView.ts
@@ -7,13 +7,13 @@
 
 import NuclearDecayScreenView, { NuclearDecayScreenViewOptions } from '../../../../nuclear-decay-common/js/view/NuclearDecayScreenView.js';
 import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
-import BetaDecayModel, { BDSelectableIsotopes } from '../model/BetaDecayModel.js';
+import BetaDecayModel from '../model/BetaDecayModel.js';
 
 type SelfOptions = EmptySelfOptions;
 
 export type BetaDecayScreenViewOptions = SelfOptions & NuclearDecayScreenViewOptions;
 
-export default class BetaDecayScreenView extends NuclearDecayScreenView<BDSelectableIsotopes> {
+export default class BetaDecayScreenView extends NuclearDecayScreenView {
   public constructor( model: BetaDecayModel, providedOptions: BetaDecayScreenViewOptions ) {
     const options = optionize<BetaDecayScreenViewOptions, SelfOptions, NuclearDecayScreenViewOptions>()( {
       // Default options go here
